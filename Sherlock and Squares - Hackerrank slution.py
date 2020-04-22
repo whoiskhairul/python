56672e20d370a1b5f2cdec8e1199c7f285751d8e
Khairul Islam <whoiskhairul@gmail.com>
2020-04-22 21:08:58 +0600

    hackerrank problem solution.

---
+++ Sherlock and Squares - Hackerrank slution.py
@@ -0,0 +1,14 @@
+import math
+n = int(input())
+for i in range(n):
+    a , b = input().split()
+    a = int( a )
+    b = int( b )
+
+    x = int(math.sqrt(a))
+    y = int(math.sqrt(b))
+
+    if (a == x*x):
+        print(y-x+1)
+    else:
+        print(y-x)
