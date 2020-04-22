ec4d949a7af6a7a1163d18fcbbf542879857fc3a
Khairul Islam <whoiskhairul@gmail.com>
2020-04-23 00:05:39 +0600

    hackerrank problem solution.

---
+++ sock merchant hackerrank.py
@@ -0,0 +1,9 @@
+n = int(input())
+ar = list(map(int, input().rstrip().split()))
+a = []
+sum = 0
+a = list(dict.fromkeys(ar))
+for i in range(len(a)):
+    b = ar.count(a[i])//2
+    sum = sum+b
+print(sum)
