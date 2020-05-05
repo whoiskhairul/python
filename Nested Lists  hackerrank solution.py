32c81f5e94c00a8af837fc6e136d045d24c33ae9
Khairul Islam <whoiskhairul@gmail.com>
2020-05-05 18:14:22 +0600

    hackerrank problem solution.(Better solution)

--- Nested Lists  hackerrank solution.py
+++ Nested Lists  hackerrank solution.py
@@ -1,29 +1,14 @@
 n = int(input())
-s = 101
 arr = []
 for i in range(n):
     name = input()
-    name = name + ' '
     score = float(input())
-    arr.append([name,score])
-for i in range(n):
-    if arr[i][1] < s :
-        s = arr[i][1]
-for i in range(n):
-    if arr[i][1] == s :
-        arr[i].pop()
-        arr[i].pop()
-s = 101
-arr = [x for x in arr if x != []]
-
+    arr.append([score,name])
+arr = sorted(arr)
+s = arr[0][0]
+while (arr[0][0] == s ):
+    arr.pop([0][0])
+s = arr[0][0]
 for i in range(len(arr)):
-    if arr[i][1] < s :
-        s = arr[i][1]
-        result = ''
-        result = result + arr[i][0]
-    elif arr[i][1] == s:
-        result = result+arr[i][0]
-arr = sorted(result.split())
-for i in range(len(arr)):
-    print(arr[i])
-
+    if(arr[i][0] == s ):
+        print(arr[i][1])
