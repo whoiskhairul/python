edd0564a511a12584a2a7a7d65cf1bb59f5a4710
Khairul Islam <whoiskhairul@gmail.com>
2020-05-08 02:49:53 +0600

    hackerrank problem solution.

---
+++ The Minion Game hackerrank solution.py
@@ -0,0 +1,19 @@
+def minion_game(string):
+    vowel = 0
+    consonant = 0
+    for i in range(len(string)):
+
+        if(string[i] in 'AEIOU'):
+            vowel = vowel + len(string) - i
+        else:
+            consonant = consonant + len(string) - i
+    if vowel < consonant :
+        print("Stuart {}".format(consonant))
+    elif vowel > consonant :
+        print("Kevin {}".format(vowel))
+    else:
+        print('Draw')
+
+if __name__ == '__main__':
+    s = input()
+    minion_game(s)
