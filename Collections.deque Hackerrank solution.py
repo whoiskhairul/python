cbaa2a6f49a9f12ab8dc52eeb746a9d880e0c36c
Khairul Islam <whoiskhairul@gmail.com>
2020-05-10 16:37:26 +0600

    hackerrank problem solution.

---
+++ Collections.deque Hackerrank solution.py
@@ -0,0 +1,17 @@
+from collections import deque
+d = deque()
+
+for _ in range(int(input())):
+    q = input()
+    if  any(i.isdigit() for i in q):
+        p,n = q.split()
+        if p == 'append':
+            d.append(n)
+        elif p == 'appendleft':
+            d.appendleft(n)
+    elif q == 'pop':
+        d.pop()
+    elif q == 'popleft':
+        d.popleft()
+for i in range(len(d)):
+    print(d[i],end=' ')
