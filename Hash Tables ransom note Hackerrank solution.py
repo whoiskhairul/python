77ef6bd9ced1dc96859de28f7a06d460c9762b25
Khairul Islam <whoiskhairul@gmail.com>
2020-05-12 02:20:50 +0600

    hackerrank problem solution.

---
+++ Hash Tables ransom note Hackerrank solution.py
@@ -0,0 +1,13 @@
+from collections import Counter
+def checkMagazine(magazine, note):
+    mag = Counter(magazine)
+    n = Counter(note)
+    if(n-mag == {}):print('Yes')
+    else:print('No')
+
+
+if __name__ == '__main__':
+    mn = input().split()
+    magazine = input().rstrip().split()
+    note = input().rstrip().split()
+    checkMagazine(magazine, note)
