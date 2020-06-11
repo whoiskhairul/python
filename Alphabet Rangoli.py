661061cd29776ae63b9208deecfffe9ea1c8aa80
Khairul Islam <whoiskhairul@gmail.com>
2020-06-11 22:24:16 +0600

    hackerrank problem solution.

---
+++ Alphabet Rangoli.py
@@ -0,0 +1,26 @@
+from string import ascii_lowercase
+n = int(input())
+s = ascii_lowercase
+s = s[:n]
+und = n*2-1 + n*2-2
+p = s[n-1]
+
+temp = []
+print(p.center(und,'-'))
+
+for i in range(n-2,-1,-1):
+    arr = []
+    arr.append(s[i])
+    for j in range(i+1,n):
+        arr.insert(0,s[j])
+        arr.append(s[j])
+    x = '-'.join(arr).center(und,'-')
+    temp.append(x)
+    print(x)
+if temp:
+    temp.pop()
+temp.reverse()
+for i in temp:
+    print(i)
+if n != 1:
+    print(p.center(und,'-'))
---
+++ Designer Door Mat.py
@@ -0,0 +1,14 @@
+n,k = input().split()
+n = int(n)
+k = int(k)
+div = (k-3)//2
+und = '.|.'
+key = 1
+for i in range(div,2,-3):
+    print(('-'*i)+(und*key)+('-'*i))
+    key+=2
+print('WELCOME'.center(k,'-'))
+key-=2
+for i in range(3,div+1,3):
+    print( ('-'*i)+(und*key)+('-'*i) )
+    key-=2
