715ea9f784bbe4a23fc2744714744fa2aed1f957
Khairul Islam <whoiskhairul@gmail.com>
2020-06-12 20:56:34 +0600

    hackerrank problem solution.

---
+++ itertools.combinations_with_replacement().py
@@ -0,0 +1,15 @@
+from itertools import combinations_with_replacement
+
+s, k = input().split()
+k = int(k)
+s = list(s)
+s.sort()
+
+arr = []
+x = list(combinations_with_replacement(s,k))
+for j in range(len(x)):
+
+    arr.append(''.join(x[j]))
+arr.sort()
+for i in arr:
+    print(i)
