2c69f5f94bdb484e027c2c156f8e31976965a003
Khairul Islam <whoiskhairul@gmail.com>
2020-06-12 18:18:23 +0600

    hackerrank problem solution.

---
+++ Itertools-Combinations.py
@@ -0,0 +1,17 @@
+from itertools import combinations
+from collections import Counter
+
+s, k = input().split()
+k = int(k)
+c = Counter(s)
+s = list(c.elements())
+s.sort()
+
+for i in range(1, k + 1):
+    arr = []
+    x = list(combinations(s, i))
+    for j in range(len(x)):
+        arr.append(''.join(x[j]))
+
+    for i in arr:
+        print(i)
